b1dac7cf8d0ebfddd325eec5cb34e145d5dd8aa0
zyad <zyadkhaled128@gmail.com>
2023-01-14 04:08:02 +0300

    added 2 migrations

---
+++ database/migrations/2023_01_14_005002_create_hair_questions_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hair_questions', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->cascadeOnDelete()
+                ->cascadeOnUpdate();
+            $table->boolean('is_dyed');
+            $table->smallInteger('curlyness');
+            $table->integer('some_other_column');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('hair_questions');
+    }
+};
